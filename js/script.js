de1d2d50fa65338c770d050750e88990a6bb1f7c
Irek Lipko <irek@lipko.pl>
2022-02-13 03:22:12 +0100

    Add CSS and JS

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const btnTop = document.querySelector('.nav__top');
+const headerAnimation = document.querySelector('header');
+const navigation = document.querySelector('.nav');
+
+window.onscroll = () => {
+    const scrollToTop = document.documentElement.scrollTop || document.body.scrollTop;
+    if (scrollToTop > 500) {
+        btnTop.classList.add('nav__top--show');
+    } else {
+        btnTop.classList.remove('nav__top--show');
+    };
+};
+
+btnTop.addEventListener('click', () => {
+    headerAnimation.classList.remove('header__animation');
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+    setTimeout(() => {
+        headerAnimation.classList.add('header__animation');
+    }, 100);
+});
+
+navigation.addEventListener('click', (evt) => {
+    const link = evt.target.closest('.menu__link--goto');
+    if (!link) {
+        return;
+    }
+    evt.preventDefault();
+    const scrollToElement = link.getAttribute('href');
+    document.querySelector(scrollToElement).scrollIntoView({
+        behavior: 'smooth'
+    });
+});
